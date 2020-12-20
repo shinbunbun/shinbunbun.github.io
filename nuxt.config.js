42e2f4c005e5f884ea523d4e7798b890500077e4
shinbunbun <takamuneyuto@gmail.com>
2020-12-20 17:37:10 +0900

    add header & add bootstrap5

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,17 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/portfolio/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/portfolio/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css'
+      }
+    ],
+    script: [
+      {
+
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js'
+      }
     ]
   },
 
@@ -45,4 +55,4 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
   }
-}
+};
