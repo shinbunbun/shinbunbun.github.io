1271d7fad83879c6e3eb538a617ee3c3022951ce
shinbunbun <shinbunbun@users.noreply.github.com>
2021-01-01 14:40:51 +0000

    deploy: dbe052b01e4d5b7ed91883ca9bab35dc9fd1de94

---
+++ _nuxt/static/1609512050/https:/openid.net/connect/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1609512050",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{}}}("\u002Fhttps:\u002Fopenid.net\u002Fconnect"));
