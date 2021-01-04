94af504be0b9df96a1a692b702249dc1d2ad0b1b
shinbunbun <takamuneyuto@gmail.com>
2021-01-04 23:09:50 +0900

    fix responsive

--- components/LinksModal.vue
+++ components/LinksModal.vue
@@ -3,7 +3,7 @@
     <img src="images/close-black-18dp.svg" alt="" class="close-button" @click="closeModal">
     <br>
     <div class="container modal-container">
-      <div class="row row-cols-md-4 row-cols-sm-2 row-cols-1 justify-content-center">
+      <div class="row row-cols-xl-4 row-cols-md-3 row-cols-sm-2 row-cols-1 justify-content-center">
         <div v-for="card in links" :key="card.title">
           <HomeCard :data="card" @openModal="openModal" />
         </div>
