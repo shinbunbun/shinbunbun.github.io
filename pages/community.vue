6dd206ea721dc6f20c25edeeb5bd7be75909b3ac
shinbunbun <takamuneyuto@gmail.com>
2021-03-05 12:43:06 +0000

    add timeline

--- pages/community.vue
+++ pages/community.vue
@@ -13,6 +13,53 @@
       </div>
       <div class="py-2" />
       <h3>参加イベント</h3>
+      <Timeline
+        class="timeline"
+        :timeline-items="timelineItems"
+        :message-when-no-items="messageWhenNoItems"
+        color-dots="#3CB70D"
+        order="desc"
+      />
     </div>
   </div>
 </template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      messageWhenNoItems: 'データが存在しません',
+      timelineItems: [
+        {
+          from: new Date(2019, 11),
+          title: 'title',
+          description:
+          'texttexttexttexttexttext'
+        },
+        {
+          from: new Date(2018, 1),
+          title: 'title',
+          description:
+          'texttexttexttexttexttext'
+        },
+        {
+          from: new Date(2017, 6),
+          title: 'title',
+          description:
+          'texttexttexttexttexttext'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.timeline {
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
